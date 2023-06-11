f51c469e559e43d9ba1c0297fd53ef56f16ece37
mistXML <112780421+mistXML@users.noreply.github.com>
2023-06-11 12:07:52 +0000

    Create wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+return {
+  window_close_confirmation = "NeverPrompt",
+  window_background_opacity = 0.8,
+  enable_tab_bar = false,
+  color_scheme = "Catppuccin Mocha",
+  window_padding = {
+  left = 10,
+  right = 2,
+  top = 10,
+  bottom = 0,
+  },
+}
