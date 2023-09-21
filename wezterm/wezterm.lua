3dbf2e5ddf815b939258b0fe48e0349c32b59aed
mistXML <112780421+mistXML@users.noreply.github.com>
2023-09-21 16:39:56 +0000

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,9 @@
-local wezterm = require 'wezterm'
-
 return {
   window_close_confirmation = "NeverPrompt",
-  window_background_opacity = 0.8,
+  window_background_opacity = 0.9,
   enable_tab_bar = false,
   color_scheme = "Catppuccin Mocha",
+  enable_wayland = true,
   window_padding = {
   left = 10,
   right = 2,
